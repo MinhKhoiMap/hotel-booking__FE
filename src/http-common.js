a21e8494203b87fcb91880024a437532acf284d6
MinhKhoiMap <helloiamkhoi@gmail.com>
2023-04-11 10:51:48 +0700

    config https api be

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: "https://api-hotelbooking.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
